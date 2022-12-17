0b556dd4c5dd6aa5d6923345582477103388c1bd
Martin Løyche <martin.doesnt.exist@gmail.com>
2022-12-17 14:37:13 +0100

    Updating graphql-codegen to read from .env.local

---
+++ codegen.ts
@@ -0,0 +1,31 @@
+import { CodegenConfig } from '@graphql-codegen/cli';
+import * as dotenv from "dotenv";
+import * as fs from "fs";
+
+const localEnv = dotenv.parse(fs.readFileSync("./.env.local"));
+
+const URL = process.env.VITE_CMS_URL ?? localEnv.VITE_CMS_URL;
+const TOKEN = process.env.VITE_CMS_TOKEN ?? localEnv.VITE_CMS_TOKEN;
+
+const config: CodegenConfig = {
+  schema: {
+    [URL]: {
+      headers: {
+        "Authorization": `Bearer ${TOKEN}`
+      }
+    }
+  },
+  documents: ['src/**/*.tsx'],
+  generates: {
+    './src/__generated__/': {
+      preset: 'client',
+      plugins: [],
+      presetConfig: {
+        gqlTagName: 'gql',
+      }
+    }
+  },
+  ignoreNoDocuments: true,
+};
+
+export default config;
