89fb05b4de5426d8e265c71e6bd2d79de50f742a
Martin Løyche <martin.doesnt.exist@gmail.com>
2023-04-29 14:43:57 +0200

    Starting the move of certain style elements from Figma to code (font-styles and color for now)

--- src/modules/design/HeroBanner/HeroBanner.tsx
+++ src/modules/design/HeroBanner/HeroBanner.tsx
@@ -13,8 +13,16 @@ interface HeroBannerProps {
 export function HeroBanner({ className, heading, details, img }: HeroBannerProps) {
     return (
         <header className={classnames(css.header, className)}>
-            <h1 className={css.headerTitle}>{heading}</h1>
-            <p className={css.headerDetails}>{details}</p>
+            <section className={css.textSection}>
+                <h1 className={"header"}>{heading}</h1>
+                <p className={"smallHeader"}>{details}</p>
+                <div className={css.linkList}>
+                    <a href="https://github.com/JamieMoriarty">GitHub</a>
+                    <a href="https://www.linkedin.com/in/martin-søndergaard-løyche/">
+                        LinkedIn
+                    </a>
+                </div>
+            </section>
             <div className={css.headerImage}>{img}</div>
         </header>
     );
