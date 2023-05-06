42d3dcb32c524fcca3a07c905d56a6d5af4a4ada
Martin Løyche <martin.doesnt.exist@gmail.com>
2023-05-06 15:38:44 +0200

    profile-pics of various size seem to be working correctly. Still lots to be done with different screen sizes, but for a different day :)

--- src/pages/Main/components/hero/Hero.tsx
+++ src/pages/Main/components/hero/Hero.tsx
@@ -17,23 +17,23 @@ export function Hero({ className }: HeroProps) {
             details={pageContent.hero.description}
             img={
                 <picture>
-                    <source
+                    {/* <source
                         media="(min-width: 1088px)"
                         srcSet={`${
                             pageContent.hero.squareImage.url
                         }?fit=fill&w=${391}&h=${391}`}
+                    /> */}
+                    <source
+                        media="(max-width: 664px)"
+                        srcSet={`${
+                            pageContent.hero.squareImage.url
+                        }?fit=fill&w=${250}&h=${250}`}
                     />
                     <source
                         media="(max-width: 1087px)"
                         srcSet={`${
                             pageContent.hero.landscapeImage.url
                         }?&fit=fill&w=${736}&h=${Math.round(736 / 1.71)}`}
-                    />
-                    <source
-                        media="(max-width: 664px)"
-                        srcSet={`${
-                            pageContent.hero.squareImage.url
-                        }?fit=fill&w=${250}&h=${250}`}
                     />
                     <img
                         className={css.image}
