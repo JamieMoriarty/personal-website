f60e7369b3aa5eefa5723255967c251efa857982
Martin Løyche <martin.doesnt.exist@gmail.com>
2023-07-08 14:36:18 +0200

    Refactoring basic page set-up. Think I'm happy with it at this point :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { ApolloProvider } from "@apollo/client";
-import "./App.css";
 import { client } from "./api";
 import { Main } from "./pages/Main";
 
 function App() {
     return (
-        <div className="App">
-            <ApolloProvider client={client}>
-                <Main />
-            </ApolloProvider>
-        </div>
+        <ApolloProvider client={client}>
+            <Main />
+        </ApolloProvider>
     );
 }
 
